0011ae8df4d8255e219811ab5feaf40d83a34b37
Jmarkus <Jmarkus.fowler@gmail.com>
2024-09-10 00:26:32 -0400

    Only an hour tonight

--- blackJackScripts.js
+++ blackJackScripts.js
@@ -1,38 +1,47 @@
 let firstCard = 10
 let secondCard = 4
+// 1. Create a new array - cards - that contains firstCard and secondCard
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-// 2. Store the cards paragraph in a variable called cardsEl
 let cardsEl =document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
 
-
+// Create a new function called startGame() that calls renderGame()
 function startGame() {
-// 3. Render the cars on the page using this format -> "Cards: 10 4"
-cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-sumEl.textContent = "Sum: " + sum  
+    renderGame()
+}
+
+function renderGame() {
+    // 2. Refer to the cards array when rendering out the cards
+    cardsEl.textContent = "Cards: "
+     // Create a for loop that renders out all the cards instead of just two
+     for (i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+     }
+    
+
+    sumEl.textContent = "Sum: " + sum  
 if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "Wohoo! You've got Blackjack!"
     hasBlackJack = true
 } else {
-    message = "You're out of the game! 😭"
+    message = "You're out of the game!"
     isAlive = false
 }
 
 messageEl.textContent = message
 }
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+
 function newCard () {
-    console.log("Drawing a new card from the deck!")
- // 1. Create a card variable, and hard code its value to a number (2-11)
-let card = 5
-// 2. Add the new card to the sum variable
-sum += card
-// 3. Call startGame()
-startGame()
+    let card = 5
+    sum += card
+     // Push the card to the cards array
+    cards.push(card)
+    renderGame()
 }
