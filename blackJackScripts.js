8b1fe36008d1fdb7669e7ce9a2540e0e0ee00bff
Jmarkus <Jmarkus.fowler@gmail.com>
2024-09-10 23:51:21 -0400

    Learned Math Floor and Random

--- blackJackScripts.js
+++ blackJackScripts.js
@@ -1,5 +1,6 @@
-let firstCard = 10
-let secondCard = 4
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 // 1. Create a new array - cards - that contains firstCard and secondCard
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
@@ -9,6 +10,17 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let cardsEl =document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
+
+// 1. Create a function, getRandomCard(), that always returns the number 5
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+     // if 1     -> return 11
+    return Math.floor(Math.random() * 13) + 1)
+     // if 11-13 -> return 10
+     
+}
+
+
 
 // Create a new function called startGame() that calls renderGame()
 function startGame() {
@@ -37,9 +49,9 @@ if (sum <= 20) {
 
 messageEl.textContent = message
 }
-
+   // 3. Use the getRandomCard() to set the value of card
 function newCard () {
-    let card = 5
+    let card = getRandomCard()
     sum += card
      // Push the card to the cards array
     cards.push(card)
